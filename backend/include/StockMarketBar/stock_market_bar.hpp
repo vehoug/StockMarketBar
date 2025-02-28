e9b5304a708366ff36264c8bf846af8697059d29
Vegard Schøyen Houg <vegard.houg@revolve.no>
2025-03-01 00:28:18 +0100

    More dynamic price simulation functionality

--- backend/include/StockMarketBar/stock_market_bar.hpp
+++ backend/include/StockMarketBar/stock_market_bar.hpp
@@ -1,10 +1,13 @@
 #ifndef STOCK_MARKET_BAR_HPP
 #define STOCK_MARKET_BAR_HPP
 
+#include <nlohmann/json.hpp>
 #include <unordered_map>
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <chrono>
+#include <thread>
 #include <string>
 #include <random>
 
@@ -15,13 +18,22 @@ public:
     ~StockMarketBar() = default;
 
     void updatePrices();
+    void triggerEvent();
     void savePricesToJSON();
 
 private:
+    std::unordered_map<std::string, double> drink_prices;
+    std::unordered_map<std::string, double> trends;
+    std::unordered_map<std::string, double> volatilities;
+
     std::mt19937 random_number_generator;
     std::uniform_real_distribution<double> step_distance;
-    std::unordered_map<std::string, double> stock_prices;
+    std::uniform_real_distribution<double> event_distance;
+    std::uniform_int_distribution<int> event_trigger_distance;
+    std::uniform_real_distribution<double> momentum_distance;
+    
     const std::string outputFile;
+    int tick_counter;
 
 };
 
